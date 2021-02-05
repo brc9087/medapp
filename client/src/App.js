ffa68b9da7646eded462024d3b2f50ef29ca1f3b
joemc9011 <joemc9011@gmail.com>
2021-02-04 20:55:37 -0500

    logo

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.pviknlkvf 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-      
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
