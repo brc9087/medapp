fca05b18c51356a2b0cb8fc65faddbe24e42f0db
Brian Cardines <bcardines811@gmail.com>
2021-02-16 21:49:42 -0500

    resultsprintyayy

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,46 +1,69 @@
-import axios from "axios"
+import axios from "axios";
 
 export default {
-    // Get book from google search 
-    diagnosis: function(id) {
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
 
-    const options = {
-        method: 'GET',
-        url: 'https://priaid-symptom-checker-v1.p.rapidapi.com/issues/' + id + '/info',
-        params: { language: 'en-gb' },
-        headers: {
-            'x-rapidapi-key': '55a082967dmsh01d187363e4cebcp1cde3ajsn6d808025672e',
-            'x-rapidapi-host': 'priaid-symptom-checker-v1.p.rapidapi.com'
-        }
-    };
 
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    })
 
-    // getGoogleSearchBooks: function(query) {
-    //     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
-    // },
+// import axios from "axios"
 
-    // // Gets all books
-    // getBooks: function () {
-    //     return axios.get("/api/books");
-    // },
+// export default {
+//     // Get book from google search 
+//     diagnosis: function(id) {
 
-    // // Gets the book with the given id
-    // getBook: function (id) {
-    //     return axios.get("/api/books/" + id);
-    // },
+//     const options = {
+//         method: 'GET',
+//         url: 'https://priaid-symptom-checker-v1.p.rapidapi.com/issues/' + id + '/info',
+//         params: { language: 'en-gb' },
+//         headers: {
+//             'x-rapidapi-key': '55a082967dmsh01d187363e4cebcp1cde3ajsn6d808025672e',
+//             'x-rapidapi-host': 'priaid-symptom-checker-v1.p.rapidapi.com'
+//         }
+//     };
 
-    // // Saves a book to the database
-    // saveBook: function (savedBooks) {
-    //     return axios.post("/api/books", savedBooks);
-    // },
+//     axios.request(options).then(function (response) {
+//         console.log(response.data);
+//     }).catch(function (error) {
+//         console.error(error);
+//     })
 
-    // // Deletes the book with the given id
-    // deleteBook: function (id) {
-    //     return axios.delete("/api/books/" + id);
-    // }
-    }}
+//     // getGoogleSearchBooks: function(query) {
+//     //     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+//     // },
+
+//     // // Gets all books
+//     // getBooks: function () {
+//     //     return axios.get("/api/books");
+//     // },
+
+//     // // Gets the book with the given id
+//     // getBook: function (id) {
+//     //     return axios.get("/api/books/" + id);
+//     // },
+
+//     // // Saves a book to the database
+//     // saveBook: function (savedBooks) {
+//     //     return axios.post("/api/books", savedBooks);
+//     // },
+
+//     // // Deletes the book with the given id
+//     // deleteBook: function (id) {
+//     //     return axios.delete("/api/books/" + id);
+//     // }
+//     }}
