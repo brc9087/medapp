ca8f3c42dee5c488fa677e740518018552c9ca2a
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2021-02-20 14:41:57 -0500

    bodyparts updated

--- client/src/pages/BodyParts.js
+++ client/src/pages/BodyParts.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Head from "../../src/images/head.PNG"
 import Belly from "../../src/images/belly.PNG"
+import LeftArm from "../../src/images/LeftArm.PNG"
+import LeftLeg from "../../src/images/LeftLeg.PNG"
+import RightArm from "../../src/images/RightArm.PNG"
+import RightLeg from "../../src/images/RightLeg.PNG"
 
 function BodyParts() {
     return (
@@ -14,8 +18,28 @@ function BodyParts() {
                 <img src={Belly} alt="" />
             </div>
 
+            <div>
+                <img src={LeftArm} alt="" />
+            </div>
+
+            <div>
+                <img src={LeftLeg} alt="" />
+            </div>
+
+            <div>
+                <img src={RightArm} alt="" />
+            </div>
+
+            <div>
+                <img src={RightLeg} alt="" />
+            </div>
+
+
+
         </div>
     )
 };
+
+
 
 export default BodyParts;
