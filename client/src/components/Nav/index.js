8f59f259e468f4409ae2172b969fefe5b22f7de4
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2021-02-20 13:31:40 -0500

    index.js fixed

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,27 +1,47 @@
 import React from "react";
 
 const styles = {
-    navBar: {
-        backgroundColor: "navy"
+    navbar: {
+        backgroundColor: "white",
     },
-    font: {
-        color: "white",
-        fontSize: "14pt",
-        fontStyle: "italic"
-
-    },
-    text: {
-        color: "pink"
+    navbaritems: {
+        color: "black"
     }
 }
-
-function Nav() {
+function Nav(props) {
     return (
-        <nav style={styles.navBar} className="navbar navbar-expand-lg">
-            <a style={styles.font} className="navbar-brand" href="/">
-                <h2 className="text-white">OnlySymps</h2>
-            </a>
-            <div id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item" id="search">
-                        <a style={styles.text} className="nav-link" href="/">Search</a>
+<nav className="navbar" style = {styles.navbar} role="navigation" aria-label="main navigation">
+  <div className="navbar-brand">
+    <a className="navbar-item" href= "">
+      <img src="/logo/steth.png" width="35" height="30"/>
+    </a>
+    <a role="button" className="navbar-burger"aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+  <div id="navbarBasicExample"  className="navbar-menu">
+    <div className="navbar-start">
+      <a style = {styles.navbaritems} className="navbar-item">
+         Symptoms
+      </a>
+      <a style = {styles.navbaritems} className="navbar-item">
+       Doctors
+      </a>
+      <a  style = {styles.navbaritems} className="navbar-item">
+       Results
+      </a>
+    </div>
+    </div>
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a  style = {styles.navbaritems} className = "navbar-item" onClick = {props.button}>Sign out</a>
+        </div>
+      </div>
+    </div>
+</nav>
+    );
+}
+export default Nav;
