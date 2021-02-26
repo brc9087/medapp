d5d51c2bbc53f9977bbece78462e0c89357f34fa
joemc9011 <joemc9011@gmail.com>
2021-02-25 19:29:45 -0500

    profile route

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import Nav from "../components/Navbar/Nav";
+import Header from "../components/Header/Header";
 
 import UserContext from "../utils/UserContext";
 
@@ -8,25 +10,82 @@ const ProfilePage = () => {
   const user = useContext(UserContext);
   const {photoURL, displayName, email} = user;
 
+  const styles = {
+    body: {
+    postion: "fixed",
+      width: "100%",
+      height: "100%",
+      backgroundImage: 'linear-gradient(#2a3342, #3e5c90)',
+      padding: "80px",
+      backgroundColor: "#038989",
+      color: "white",
+      textAlign: "center",
+    },
+    tiles: {
+      color: "white"
+    },
+    parent: {
+      backgroundColor: "#4ac8c8"
+    },
+    child: {
+      backgroundColor: "#0d9f9f"
+
+    }
+
+
+   
+  }
+
   return (
-    <div className = "mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
-      <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
-        <div
-          style={{
-            background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
-            backgroundSize: "cover",
-            height: "200px",
-            width: "200px"
-          }}
-          className="border border-blue-300"
-        ></div>
-        <div className = "md:pl-4">
-        <h2 className = "text-2xl font-semibold">{displayName}</h2>
-        <h3 className = "italic">{email}</h3>
-        </div>
-      </div>
-      <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {auth.signOut()}}>Sign out</button>
+    <>
+    <Header Logo="/logo/logo.png" />
+    <Nav 
+    name = {displayName + "'s"}
+    button= {() => { auth.signOut() }} />
+
+    <div className = "main"  style={styles.body}>
+      <div >
+    
+      <div className="tile is-ancestor">
+          
+
+
+          <div className="tile is-parent" style = {styles.tiles}>
+            <article className="tile is-child notification " style = {styles.parent}>
+            <h1 className="greeting"> Hope you feel better soon {displayName}!</h1>
+              <div className="content">
+
+
+                <div className="tile is-parent is-vertical">
+                  <article className="tile is-child notification " style = {styles.child}>
+                    <p>Tell us what's wrong</p>
+                    <p >Top tile</p>
+                  </article>
+                  <article className="tile is-child notification "style = {styles.child}>
+                    <p>Past sick days</p>
+                    <p>Bottom tile</p>
+                  </article>
+                </div>
+                <div className="tile is-parent">
+                  <article className="tile is-child notification " style = {styles.child}>
+                    <p>Find a doctor</p>
+                    <p >With an image</p>
+
+                  </article>
+                </div>
+
+
+
+              </div>
+            </article>
+          </div>
+
+
     </div>
+      </div>
+      
+    </div>
+    </>
   ) 
 };
 
