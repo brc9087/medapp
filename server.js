c52d3ac49a78a974aa4c6da4608315954c2facca
Islam4049 <70667310+Islam4049@users.noreply.github.com>
2021-03-06 09:12:46 -0500

    change in server

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 const express = require("express");
 const mongoose = require ("mongoose");
 const routes= require ("./routes")
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname));
-  });
-
-
 // Define API routes here
 app.use(routes)
 
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 //Connet to mongoose
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/onlysymps',
@@ -40,5 +38,3 @@ mongoose.connect(
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
-
