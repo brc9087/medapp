bd717bd23e536686f806dcc1c0020f5df6579de6
Brian Cardines <bcardines811@gmail.com>
2021-03-06 21:50:42 -0500

    env

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-
-// This file empties the Books collection and inserts the books below
+require("dotenv").config()
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/onlysymps"
+  "mongodb://localhost/onlysymps", console.log("wazzaapp")
 );
 
 const diagnosisSeed = [
